fdbc4aea9123c4de54f9fa0011af1f80c608f1ad
sharfuddin.zishan <sharfuddin.ahamed91@gmail.com>
2023-01-12 00:25:05 +0600

    Create user with email and password using React Bootstrap

--- src/component/RegisterReactBootstrap.js
+++ src/component/RegisterReactBootstrap.js
@@ -39,12 +39,12 @@ const RegisterReactBootstrap = () => {
             <Form className='w-50 mx-auto' onSubmit={handleSignUp}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" onBlur={handleInput} />
+                    <Form.Control type="email" placeholder="Enter email" onBlur={handleInput} required />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" onBlur={handleInput} />
+                    <Form.Control type="password" placeholder="Password" onBlur={handleInput} required />
                 </Form.Group>
                 <Button variant="primary" type="submit" disabled={loading ? true : false}>
                     Submit
